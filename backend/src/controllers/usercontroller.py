1212e76b557ac20658a80977eb77956a18063725
Vemana9 <vera25@student.bth.se>
2026-05-14 22:42:02 +0200

    Verification: Add docstrings and boundary tests for UserController

--- backend/src/controllers/usercontroller.py
+++ backend/src/controllers/usercontroller.py
@@ -9,20 +9,11 @@ class UserController(Controller):
         super().__init__(dao=dao)
 
     def get_user_by_email(self, email: str):
-        """Given a valid email address of an existing account, return the user object contained in the database associated 
-        to that user. For now, do not assume that the email attribute is unique. Additionally print a warning message containing the email
-        address if the search returns multiple users.
+        """
+        Retrieves a user from the database by their email address.
         
-        parameters:
-            email -- an email address string 
-
-        returns:
-            user -- the user object associated to that email address (if multiple users are associated to that email: return the first one)
-            None -- if no user is associated to that email address
-
-        raises:
-            ValueError -- in case the email parameter is not valid (i.e., conforming <local-part>@<domain>.<host>)
-            Exception -- in case any database operation fails
+        Fix applied: Added defensive check to return None if no users 
+        are found, preventing IndexError during result indexing.
         """
 
         if not re.fullmatch(emailValidator, email):
